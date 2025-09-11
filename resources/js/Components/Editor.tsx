d4e06d6206410f21f504a2facbe070507a584cf7
AlnThea <dahlan.fauzi1991@gmail.com>
2025-09-11 20:06:08 +0700

    update

--- resources/js/Components/Editor.tsx
+++ resources/js/Components/Editor.tsx
@@ -287,7 +287,6 @@ const Editor: React.FC<EditorProps> = ({ onSave, initialData, editorRef }) => {
                             <button key={btn.id} title={btn.title} className="p-2 hover:bg-slate-600 rounded" onClick={() => handleCommand(btn.id)} dangerouslySetInnerHTML={{ __html: btn.label }} />
                         ))}
                     </div>
-
                     <div className="flex items-center space-x-2">
                         {[
                             { id: 'set-device-desktop', label: '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 5a1 1 0 0 1 1 -1h16a1 1 0 0 1 1 1v10a1 1 0 0 1 -1 1h-16a1 1 0 0 1 -1 -1v-10z" /><path d="M7 20h10" /><path d="M9 16v4" /><path d="M15 16v4" /></svg>', title: 'Desktop View' },
