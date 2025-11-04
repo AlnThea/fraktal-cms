f94e4fef1a5a1c40ed6c8c5ffad31d523b5dac94
dahlan1991 <dahlan.fauzi1991@gmail.com>
2025-11-04 18:09:13 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-//Route::get('/active-plugins', [\App\Http\Controllers\PluginController::class, 'getActivePlugins']);
