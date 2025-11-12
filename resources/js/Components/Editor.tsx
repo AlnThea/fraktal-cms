95c553dbc53e15fb4dfcbbf59ee9148b518bc6d5
dahlan1991 <dahlan.fauzi1991@gmail.com>
2025-11-12 18:17:15 +0700

    update

--- resources/js/Components/Editor.tsx
+++ resources/js/Components/Editor.tsx
@@ -343,10 +343,10 @@ const Editor: React.FC<EditorProps> = ({ onSave, initialData, editorRef }) => {
                     isSidebarLeftOpen ? 'left-[17.9rem]' : 'left-0'
                 }`}
             >
-                <div className="flex items-center w-10 text-teal-400 bg-white border-t-2 border-r-2 border-b-2 rounded-tr-xl rounded-br-xl border-teal-300">
+                <div className="flex flex-col space-y-2  pt-1 pb-1 items-center w-10 text-teal-400 bg-white border-t-2 border-r-2 border-b-2 rounded-tr-xl rounded-br-xl border-teal-300">
                     <button
                         title="Block Manager"
-                        className="p-1"
+                        className="p-1 hover:bg-teal-200 rounded-lg"
                         onClick={() => setIsSidebarLeftOpen(!isSidebarLeftOpen)}
                         dangerouslySetInnerHTML={{
                             __html: isSidebarLeftOpen
@@ -354,6 +354,26 @@ const Editor: React.FC<EditorProps> = ({ onSave, initialData, editorRef }) => {
                                 : '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-blocks"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14 4a1 1 0 0 1 1 -1h5a1 1 0 0 1 1 1v5a1 1 0 0 1 -1 1h-5a1 1 0 0 1 -1 -1z" /><path d="M3 14h12a2 2 0 0 1 2 2v3a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h3a2 2 0 0 1 2 2v12" /></svg>',
                         }}
                     />
+                    {isSidebarLeftOpen && [
+                        {
+                            id: 'core:preview',
+                            label: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" /></svg>',
+                            title: 'Preview'
+                        },
+                        {
+                            id: 'export-template',
+                            label: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14 3v4a1 1 0 0 0 1 1h4" /><path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" /><path d="M9 17h6" /><path d="M9 13h6" /></svg>',
+                            title: 'Export Code'
+                        },
+                    ].map((btn) => (
+                        <button
+                            key={btn.id}
+                            title={btn.title}
+                            className="p-1 hover:bg-teal-200 rounded-lg"
+                            onClick={() => handleCommand(btn.id)}
+                            dangerouslySetInnerHTML={{ __html: btn.label }}
+                        />
+                    ))}
                 </div>
             </div>
 
@@ -414,11 +434,14 @@ const Editor: React.FC<EditorProps> = ({ onSave, initialData, editorRef }) => {
                     </div>
                 </div>
 
-                <div className="flex flex-grow w-full">
+                <div className="flex flex-grow w-full ">
                     {/* Canvas Utama */}
-                    <div className="flex-grow editor-canvas">
-                        <div ref={editorInstanceRef}></div>
+                    <div className={`${ isSidebarLeftOpen ? 'w-full ml-72.5 ease-in-out' : 'w-full'}`}>
+                        <div className={`flex-grow editor-canvas `}>
+                            <div ref={editorInstanceRef}></div>
+                        </div>
                     </div>
+
 
                     {/* Tombol Toggle Sidebar Kanan (Hanya Muncul di Mobile) */}
                     <div className={`absolute top-20 z-40 transition-all duration-300 ease-in-out lg:hidden ${ isSidebarRightOpen ? 'right-[17.9rem]' : 'right-0' }`}>
